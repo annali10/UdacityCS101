edff7ee274460eb6c668e92c286aeda0eee349a5
Anna Li <annax.li10@gmail.com>
2013-11-17 19:00:22 -0500

    Luke helped with most problems, only thing left is fixing last test because F1 is excluded from crawl

---
+++ Lesson3/Experiment.py
@@ -0,0 +1,12 @@
+myName = [2,4,7,0]
+
+def addConcat(myName):
+    myName = myName + [14]
+    
+def addApp(myName):
+    myName.append(18)
+    
+print myName
+print addConcat(myName)
+print addApp(myName)
+
